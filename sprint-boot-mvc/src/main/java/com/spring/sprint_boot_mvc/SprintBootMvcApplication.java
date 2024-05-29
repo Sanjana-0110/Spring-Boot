2af0ba80cbe50e8d61c31d59c26b7a64f9fb7c66
Sanjana-0110 <150668402+Sanjana-0110@users.noreply.github.com>
2024-05-29 16:59:07 +0530

    Add files via upload

---
+++ sprint-boot-mvc/src/main/java/com/spring/sprint_boot_mvc/SprintBootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sprint_boot_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootMvcApplication.class, args);
+	}
+
+}
