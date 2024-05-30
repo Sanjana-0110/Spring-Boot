eba46c34ecb474ce17a766b9710aaa4823f2b45e
Sanjana-0110 <150668402+Sanjana-0110@users.noreply.github.com>
2024-05-30 09:19:22 +0530

    Delete sprint-boot-mvc directory

--- sprint-boot-mvc/src/main/java/com/spring/sprint_boot_mvc/repositories/EmpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.sprint_boot_mvc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.sprint_boot_mvc.models.Employee;
-
-public interface EmpRepository extends JpaRepository<Employee, Integer> {
-
-}
